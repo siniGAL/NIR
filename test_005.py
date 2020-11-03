97d70162a183d60bf2cfaeb93f2ba759cd0ab6b5
sinigal <buev.pa@phystech.edu>
2020-11-03 12:28:19 +0300

    mod

--- test_005.py
+++ test_005.py
@@ -4,16 +4,15 @@ import os
 from multiprocessing import Pool
 
 
-def get_image_paths(path):
+def get_pcap_paths(path):
     return (os.path.join(path, f)
             for f in os.listdir(path)
             if 'pcap' in f)
 
 
 def create_csv(files):
-    packets_data = {}
     scapy_cap = rdpcap(files)
-
+    packets_data = {}
     for idx, packet in enumerate(scapy_cap):
         string = []
         if 'IP' in packet:
@@ -27,19 +26,18 @@ def create_csv(files):
         else:
             string.insert(1, 'xxxx')
             string.insert(3, 'xxxx')
-        packets_data[idx] = string
-    table = {}
-    table[files] = packets_data
-    data_frame = pd.DataFrame(table)
-    data_frame.to_csv('%s.csv' % (files))
+        packets_data = {idx: string}
+    data_frame = pd.DataFrame(packets_data)
+    data_frame.to_csv(path_or_buf='/mnt/data/csv/' + files.replace(path, '') + '.csv')
     print(files)
 
+
 if __name__ == '__main__':
-    path = '/Users/macbookpro/Documents/Dataset/Pcaps/tor/'
+    path = '/mnt/data/test/'
 
-    files = get_image_paths(path)
+    files = get_pcap_paths(path)
 
-    pool = Pool(4)
+    pool = Pool(10)
     pool.map(create_csv, files)
     pool.close()
     pool.join()
